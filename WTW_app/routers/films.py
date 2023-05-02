caa5ef580f674951588cd168707637dfde5843d5
kpusz <kpusz@novomatic-tech.com>
2023-05-02 12:42:31 +0200

    Provide repository based on interfaces in films router and add response_model to each endpoint

--- WTW_app/routers/films.py
+++ WTW_app/routers/films.py
@@ -2,6 +2,7 @@ import typing as tp
 
 from fastapi import APIRouter, HTTPException, status
 from WTW_app.models import FilmModel, AddFilmModel
+from WTW_app.repositories.interfaces import IFilmsRepository
 from WTW_app.repositories.films_in_memory import films_in_memory
 
 films_router = APIRouter(
@@ -10,14 +11,18 @@ films_router = APIRouter(
 )
 
 
-@films_router.get("/")
+@films_router.get("/", response_model=tp.List[FilmModel])
 def get_films_list() -> tp.List[FilmModel]:
-    _films = films_in_memory.get_films()
+    films_repository: IFilmsRepository = films_in_memory
+    
+    _films = films_repository.get_films()
     return _films
 
-@films_router.post("/", status_code=status.HTTP_201_CREATED)
+@films_router.post("/", response_model=FilmModel, status_code=status.HTTP_201_CREATED)
 def add_film(film_payload: AddFilmModel) -> FilmModel:
-    _film: tp.Optional[FilmModel] = films_in_memory.add_film(
+    films_repository: IFilmsRepository = films_in_memory
+    
+    _film: tp.Optional[FilmModel] = films_repository.add_film(
         title=film_payload.title,
         year=film_payload.year,
         rate=film_payload.rate,
@@ -38,7 +43,9 @@ def add_film(film_payload: AddFilmModel) -> FilmModel:
     responses={404: {"description": "Film for given film_id not found."}},
 )
 async def delete_film(film_id: int):
-    _film: tp.Optional[FilmModel] = films_in_memory.remove_film(film_id=film_id)
+    films_repository: IFilmsRepository = films_in_memory
+    
+    _film: tp.Optional[FilmModel] = films_repository.remove_film(film_id=film_id)
     
     if not _film:
         raise HTTPException(
