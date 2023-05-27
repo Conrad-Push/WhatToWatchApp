4093873af7dc2434d2414bfe6270daa06836fe95
kpusz <kpusz@novomatic-tech.com>
2023-05-27 13:18:19 +0200

    Update DB_PORT in db settings

--- WTW_app/settings/db_settings.py
+++ WTW_app/settings/db_settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     db_username: str = Field("DB_USERNAME", env="DB_USERNAME")
     db_password: str = Field("Password", env="DB_PASSWORD")
     db_host: str = Field("DB_HOST", env="DB_HOST")
-    db_port: str = Field("5000", env="DB_PORT")
+    db_port: str = Field("5432", env="DB_PORT")
     db_name: str = Field("DB_NAME", env="DB_NAME")
 
 
