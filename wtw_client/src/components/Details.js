b917c90fa91289b9070185cc0d32f6180fe05cea
kpusz <kpusz@novomatic-tech.com>
2023-06-06 13:52:04 +0200

    Add details editing functionality

--- wtw_client/src/components/Details.js
+++ wtw_client/src/components/Details.js
@@ -5,6 +5,9 @@ import axios from "axios";
 function Details() {
   const [film, setFilm] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [editedDirector, setEditedDirector] = useState("");
+  const [editedDescription, setEditedDescription] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const { film_id } = useParams();
   const navigate = useNavigate();
 
@@ -32,6 +35,56 @@ function Details() {
 
   const { title, year, rate, img_url, details } = film;
 
+  const handleDirectorChange = (event) => {
+    setEditedDirector(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setEditedDescription(event.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (editedDirector || editedDescription) {
+      setErrorMessage("");
+
+      try {
+        const updatedDetails = {};
+
+        if (editedDirector !== "") {
+          updatedDetails.director = editedDirector;
+        }
+
+        if (editedDescription !== "") {
+          updatedDetails.description = editedDescription;
+        }
+
+        const response = await axios.patch(
+          `/postgresql/details/${details.details_id}`,
+          updatedDetails
+        );
+
+        if (response.status === 200) {
+          setFilm((prevFilm) => ({
+            ...prevFilm,
+            details: {
+              ...prevFilm.details,
+              ...updatedDetails,
+            },
+          }));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      setEditedDirector("");
+      setEditedDescription("");
+    } else {
+      setErrorMessage("Please fill at least one field");
+    }
+  };
+
   const handleGoBack = () => {
     navigate("/postgresql");
   };
@@ -43,7 +96,7 @@ function Details() {
           <div className="photo-frame">
             <img className="photo-details" src={img_url} alt={title} />
           </div>
-          <div>
+          <div className="details-frame">
             <div id="film-title">{title}</div>
             <div className="details-content">
               <div>
@@ -62,6 +115,26 @@ function Details() {
               </div>
             </div>
           </div>
+        </div>
+        <div className="add-film-card">
+          <form className="card-content">
+            <input
+              type="text"
+              placeholder="Modify director..."
+              value={editedDirector}
+              onChange={handleDirectorChange}
+            />
+            <input
+              type="text"
+              placeholder="Modify description..."
+              value={editedDescription}
+              onChange={handleDescriptionChange}
+            />
+            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+            <button className="edit-button" onClick={handleSubmit}>
+              Save Changes
+            </button>
+          </form>
         </div>
       </div>
       <div className="back-button-container">
